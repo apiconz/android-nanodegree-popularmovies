c7a001185749f3e64f0a4bef37732ed299a4cffb
Armando <apiconz@gmail.com>
2015-07-18 01:26:29 -0500

    Initial commit

---
+++ app/src/main/java/pe/apiconz/android/nanodegree/popularmovies/util/Constants.java
@@ -0,0 +1,10 @@
+package pe.apiconz.android.nanodegree.popularmovies.util;
+
+/**
+ * Created by Armando on 7/28/2015.
+ */
+public class Constants {
+    public static final String MOVIES_API_KEY="c2cdb027bf4546d03077621c8d7b3aec";
+    public static final String DISCOVER_MOVIE_URL="http://api.themoviedb.org/3/discover/movie";
+    public static final String POSTER_BASE_URL="http://image.tmdb.org/t/p/w185";
+}
---
+++ app/src/main/java/pe/apiconz/android/nanodegree/popularmovies/view/MainActivityFragment.java
@@ -0,0 +1,24 @@
+package pe.apiconz.android.nanodegree.popularmovies.view;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pe.apiconz.android.nanodegree.popularmovies.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
